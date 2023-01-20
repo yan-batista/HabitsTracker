6c756dc982e77e46c68e285062359d316a5c2b15
Yan  Batista <y.batista@gsuite.iff.edu.br>
2023-01-20 03:18:05 -0300

    db seed, routes, schemas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,21 +1,10 @@
 import Fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
+import { appRoutes } from "./routes";
 
 const app = Fastify();
-const prisma = new PrismaClient();
 const PORT = 5173;
 
-app.get("/", () => {
-  const habits = prisma.habit.findMany({
-    where: {
-      title: {
-        startsWith: "Beber",
-      },
-    },
-  });
-
-  return habits;
-});
+app.register(appRoutes);
 
 app
   .listen({
