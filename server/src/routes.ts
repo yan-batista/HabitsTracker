0b246bcccef3c8b5939a057170486e16e6ef5ec2
Yan  Batista <y.batista@gsuite.iff.edu.br>
2023-01-21 02:17:53 -0300

    rota de toggle de habito completo e rota de resumo de dias

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -76,4 +76,93 @@ export async function appRoutes(app: FastifyInstance) {
       completedHabits,
     };
   });
+
+  app.patch("/habits/:id/toggle", async (request) => {
+    const toggleHabitParams = z.object({
+      id: z.string().uuid(),
+    });
+
+    // pega o id do hábito
+    const { id } = toggleHabitParams.parse(request.params);
+
+    // de acordo com as regras de negócio, só podemos alterar se foi completo na data atual
+    // não podemos alterar hábitos de datas passadas ou futuras
+
+    // procura o dia atual na tabela de dias
+    // um dia só é criado na tabela de dias se um hábito for completo nesse dia
+    const today = dayjs().startOf("day").toDate();
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      },
+    });
+
+    // se não encontrarmos esse dia na tabela, quer dizer que não foram completos hábitos
+    // já que essa rota é para completar hábito, vamos criar esse dia na tabela
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        },
+      });
+    }
+
+    // procuramos o dayHabbit para ver se esse habito já foi marcado como completo antes
+
+    const dayHabbit = await prisma.dayHabbit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      },
+    });
+
+    // se esse habito foi marcado como completo antes, então essa rota irá remove-lo
+    if (dayHabbit) {
+      await prisma.dayHabbit.delete({
+        where: {
+          id: dayHabbit.id,
+        },
+      });
+    } else {
+      // se esse habito NÃO foi marcado como completo antes, então essa rota irá cria-lo
+      // vamos criar uma entrada na tabela dayHabbit usando o dia de hoje e o hábito do param
+      // ou seja, estamos marcando o hábito como completo
+      await prisma.dayHabbit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      });
+    }
+  });
+
+  app.get("/summary", async () => {
+    // retornar uma data, quantos hábitos estão disponíveis e quantos foram completos
+    const summary = await prisma.$queryRaw`
+      SELECT
+        D.id,
+        D.date,
+        (
+          SELECT
+            cast(count(*) as float)
+          FROM day_habits DH
+          WHERE DH.day_id = D.id
+        ) as completed,
+        (
+          SELECT
+            cast(count(*) as float)
+          FROM habit_week_days HWD
+          JOIN habits H
+            on H.id = HWD.habit_id
+          WHERE
+            HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+            AND H.created_at <= D.date
+        ) as amount
+      FROM days D
+    `;
+
+    return summary;
+  });
 }
